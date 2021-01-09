09f66bf8cb6c62cb47a6b30d496ce26a40259893
hobbyquaker <bastiraff@googlemail.com>
2021-01-09 17:01:56 +0100

    implement sortable for static child position

--- www/src/feezal-sidebar-inspector.js
+++ www/src/feezal-sidebar-inspector.js
@@ -1,6 +1,7 @@
 import {PolymerElement, html} from '@polymer/polymer/polymer-element';
 
 import 'dragselect';
+import sortable from 'html5sortable/dist/html5sortable.es.js';
 
 import '@polymer/iron-pages/iron-pages';
 import '@polymer/paper-tabs/paper-tabs';
@@ -92,7 +93,7 @@ class FeezalSidebarInspector extends PolymerElement {
                 
                 #editor-form {
                     margin: 12px;
-                }                
+                }        
 
             </style>
             
@@ -182,19 +183,27 @@ class FeezalSidebarInspector extends PolymerElement {
         feezal.app.shadowRoot.querySelector('#grid').style.display = value ? 'block' : 'none';
     }
 
-    _viewChanged() {
-        if (!feezal.ready) {
-            return;
+    _updateSelection() {
+        const view = feezal.getView(this.view);
+        const selectedElems = [...view.querySelectorAll('.feezal-selected')];
+        if (selectedElems.length > 0) {
+            this.set('selectedElems', selectedElems);
+            this.set('viewSelected', false);
+        } else {
+            this.set('selectedElems', [view]);
+            this.set('viewSelected', true);
         }
-        // Console.log('feezal-editor _viewChanged', this.view)
+    }
 
-        const views = [...feezal.site.querySelectorAll('feezal-view')].map(v => v.name);
+    _initSortable(view) {
+        sortable(view, {
+            items: '.feezal-element',
+            forcePlaceholderSize: true,
+            placeholderClass: 'feezal-placeholder'
+        });
+    }
 
-        if (!views.includes(this.view)) {
-            this.view = views[0];
-            location.hash = '/' + this.view;
-        }
-
+    _initDragSelect() {
         const view = feezal.getView(this.view);
 
         if (!this.dragselect) {
@@ -220,32 +229,54 @@ class FeezalSidebarInspector extends PolymerElement {
                 selector,
                 selectedClass: 'feezal-selected',
                 onDragStartBegin: element => {
+                    console.log('onDragStartBegin')
                     if (element.target.tagName !== 'FEEZAL-VIEW') {
                         this.dragselect[this.view].break();
                         return false;
                     }
                 },
-                callback: elements => {
-                    const selectedElems = [...view.querySelectorAll('.feezal-selected')];
-                    if (selectedElems.length > 0) {
-                        this.set('selectedElems', selectedElems);
-                        this.set('viewSelected', false);
-                    } else {
-                        this.set('selectedElems', [view]);
-                        this.set('viewSelected', true);
-                    }
+                callback: () => {
+                    this._updateSelection();
                 }
             });
         }
 
         this.dragselect[this.view].start();
 
+    }
+
+    _viewChanged() {
+        if (!feezal.ready) {
+            return;
+        }
+        const view = feezal.getView(this.view);
+
+        console.log('feezal-editor _viewChanged', this.view, view.childPosition);
+
+        const views = [...feezal.site.querySelectorAll('feezal-view')].map(v => v.name);
+
+        if (!views.includes(this.view)) {
+            this.view = views[0];
+            location.hash = '/' + this.view;
+        }
+
+        switch (view.childPosition) {
+
+            case 'static':
+                this._initSortable(view);
+                break;
+
+            case 'absolute':
+                this._initDragSelect();
+                break;
+        }
+
         this.currentView = [view];
 
         [...view.children].forEach(element => {
             if (element.localName.startsWith('feezal-element-') && !element.feezalEditable) {
                 this.initElem(element);
-            } else {
+            } else if (this.dragselect[this.view]) {
                 this.dragselect[this.view].removeSelection(element);
             }
         });
@@ -372,11 +403,11 @@ class FeezalSidebarInspector extends PolymerElement {
                 changes.push('top');
             }
 
-            if (event.dx && this.selectedElems.map(element => element.style.left).every(value => value === this.selectedElems[0].style.left)) {
+            if (dx && this.selectedElems.map(element => element.style.left).every(value => value === this.selectedElems[0].style.left)) {
                 changes.push('left');
             }
 
-            if (event.dy && this.selectedElems.map(element => element.style.top).every(value => value === this.selectedElems[0].style.top)) {
+            if (dy && this.selectedElems.map(element => element.style.top).every(value => value === this.selectedElems[0].style.top)) {
                 changes.push('top');
             }
 
@@ -518,6 +549,8 @@ class FeezalSidebarInspector extends PolymerElement {
             return;
         }
 
+        const absolute = element.parentNode.childPosition === 'absolute';
+
         element.feezalEditable = true;
         element.classList.add('feezal-editable');
         const elementOptions = window.customElements.get(element.localName) && window.customElements.get(element.localName).feezal || {};
@@ -527,170 +560,194 @@ class FeezalSidebarInspector extends PolymerElement {
             return;
         }
 
+
         if (created && elementOptions.defaultStyle) {
             Object.assign(element.style, elementOptions.defaultStyle);
         }
 
+        if (absolute) {
+            this.initAbsolute(element, elementOptions);
+        } else {
+            this.initStatic(element, elementOptions);
+        }
+
+    }
+    initStatic(element, elementOptions) {
+        element.addEventListener('click', (event) => {
+            [...feezal.view.querySelectorAll('.feezal-selected')].forEach(element => {
+                element.classList.remove('feezal-selected');
+            })
+            this.selectElement(element);
+        });
+    }
+    initAbsolute(element, elementOptions) {
         interact(element)
-            .draggable({
-                restrict: {
-                    restriction: () => this.dragRect,
-                    elementRect: {top: 0, left: 0, bottom: 1, right: 1}
-                },
-                snap: {
-                    targets: [
-                        (x, y) => this._snap(x, y)
-                    ],
-                    relativePoints: [
-                        {x: 0, y: 0}, // Snap relative to the element's top-left,
-                        {x: 1, y: 0}, // Snap relative to the element's top-left,
-                        {x: 0, y: 1}, // Snap relative to the element's top-left,
-                        {x: 1, y: 1} // Snap relative to the element's top-left,
-                    ]
+        .draggable({
+            restrict: {
+                restriction: () => this.dragRect,
+                elementRect: {top: 0, left: 0, bottom: 1, right: 1}
+            },
+            snap: {
+                targets: [
+                    (x, y) => this._snap(x, y)
+                ],
+                relativePoints: [
+                    {x: 0, y: 0}, // Snap relative to the element's top-left,
+                    {x: 1, y: 0}, // Snap relative to the element's top-left,
+                    {x: 0, y: 1}, // Snap relative to the element's top-left,
+                    {x: 1, y: 1} // Snap relative to the element's top-left,
+                ]
+            }
+        })
+        .resizable({
+            edges: {left: true, right: true, bottom: true, top: true},
+            restrictEdges: {
+                outer: 'parent'
+            },
+            margin: 5,
+            restrictSize: {
+                min: {
+                    width: (elementOptions.restrict && elementOptions.restrict.minWidth) || 12,
+                    height: (elementOptions.restrict && elementOptions.restrict.minHeight) || 12
                 }
-            })
-            .resizable({
-                edges: {left: true, right: true, bottom: true, top: true},
-                restrictEdges: {
-                    outer: 'parent'
-                },
-                margin: 5,
-                restrictSize: {
-                    min: {
-                        width: (elementOptions.restrict && elementOptions.restrict.minWidth) || 12,
-                        height: (elementOptions.restrict && elementOptions.restrict.minHeight) || 12
+            },
+            snapSize: {
+                targets: [
+                    (x, y) => this._snapSize(x, y)
+                ]
+            }
+        })
+        .on('dragstart', event => {
+            const view = feezal.getView(this.view);
+            const viewRect = view.getBoundingClientRect();
+            const targetRect = event.target.getBoundingClientRect();
+            this.dragElement = event.target;
+
+            if (!event.target.classList.contains('feezal-selected')) {
+                this.dragselect[this.view].setSelection(event.target, true);
+            }
+
+            const groupRect = {
+                top: targetRect.top,
+                left: targetRect.left,
+                bottom: targetRect.bottom,
+                right: targetRect.right
+            };
+
+            this.selectedElems.forEach(element_ => {
+                if (event.target !== element_) {
+                    const elementRect = element_.getBoundingClientRect();
+                    if (elementRect.top < groupRect.top) {
+                        groupRect.top = elementRect.top;
                     }
-                },
-                snapSize: {
-                    targets: [
-                        (x, y) => this._snapSize(x, y)
-                    ]
-                }
-            })
-            .on('dragstart', event => {
-                const view = feezal.getView(this.view);
-                const viewRect = view.getBoundingClientRect();
-                const targetRect = event.target.getBoundingClientRect();
-                this.dragElement = event.target;
 
-                if (!event.target.classList.contains('feezal-selected')) {
-                    this.dragselect[this.view].setSelection(event.target, true);
-                }
-
-                const groupRect = {
-                    top: targetRect.top,
-                    left: targetRect.left,
-                    bottom: targetRect.bottom,
-                    right: targetRect.right
-                };
-
-                this.selectedElems.forEach(element_ => {
-                    if (event.target !== element_) {
-                        const elementRect = element_.getBoundingClientRect();
-                        if (elementRect.top < groupRect.top) {
-                            groupRect.top = elementRect.top;
-                        }
-
-                        if (elementRect.left < groupRect.left) {
-                            groupRect.left = elementRect.left;
-                        }
-
-                        if (elementRect.bottom > groupRect.bottom) {
-                            groupRect.bottom = elementRect.bottom;
-                        }
-
-                        if (elementRect.right > groupRect.right) {
-                            groupRect.right = elementRect.right;
-                        }
+                    if (elementRect.left < groupRect.left) {
+                        groupRect.left = elementRect.left;
                     }
-                });
-                this.dragRect = {
-                    top: viewRect.top + (targetRect.top - groupRect.top),
-                    left: viewRect.left + (targetRect.left - groupRect.left),
-                    bottom: viewRect.bottom + (targetRect.bottom - groupRect.bottom),
-                    right: viewRect.right + (targetRect.right - groupRect.right)
-                };
-            })
-            .on('dragmove', event => {
-                const {target} = event;
-                this.dragselect[this.view].removeSelectables(target);
-                this._moveElems(event.dx, event.dy);
-            })
-            .on('dragend', event => {
-                setTimeout(() => {
-                    this.dragselect[this.view].addSelectables(event.target);
-                }, 10);
-                const vsnap1 = feezal.container.querySelector('#vsnap1');
-                const hsnap1 = feezal.container.querySelector('#hsnap1');
-                const vsnap2 = feezal.container.querySelector('#vsnap2');
-                const hsnap2 = feezal.container.querySelector('#hsnap2');
 
-                vsnap1.style.display = 'none';
-                hsnap1.style.display = 'none';
-                vsnap2.style.display = 'none';
-                hsnap2.style.display = 'none';
+                    if (elementRect.bottom > groupRect.bottom) {
+                        groupRect.bottom = elementRect.bottom;
+                    }
 
-                this.dragElement = null;
-                feezal.app.change();
-            })
-            .on('resizestart', event => {
-                // Console.log('resizestart', event.target)
-                this.resizeElement = event.target;
-                if (!event.target.classList.contains('feezal-selected')) {
-                    this.dragselect[this.view].setSelection(event.target, true);
+                    if (elementRect.right > groupRect.right) {
+                        groupRect.right = elementRect.right;
+                    }
                 }
-            })
-            .on('resizemove', event => {
-                const {target} = event;
-
-                const changes = [];
-
-                let x = Number.parseFloat(target.style.left.replace('px', '')) || 0; // Target.xCoord || 0;
-                let y = Number.parseFloat(target.style.top.replace('px', '')) || 0; // Target.yCoord || 0;
-
-                const {width} = event.rect;
-                const {height} = event.rect;
-
-                // Update the element's style
-                if (target.style.width !== width + 'px') {
-                    target.style.width = width + 'px';
-                    changes.push('width');
-                }
-
-                if (target.style.height !== height + 'px') {
-                    target.style.height = height + 'px';
-                    changes.push('height');
-                }
-
-                // Translate when resizing from top or left edges
-                x += event.deltaRect.left;
-                y += event.deltaRect.top;
-
-                if (target.style.left !== x + 'px') {
-                    target.style.left = x + 'px';
-                    changes.push('left');
-                }
-
-                if (target.style.top !== y + 'px') {
-                    target.style.top = y + 'px';
-                    changes.push('top');
-                }
-
-                this.shadowRoot.querySelector('feezal-sidebar-inspector-styles').setStyle(target, changes);
-            })
-            .on('resizeend', event => {
-                setTimeout(() => {
-                    this.dragselect[this.view].addSelectables(event.target);
-                }, 10);
-                this.resizeElement = null;
-                feezal.app.change();
             });
+            this.dragRect = {
+                top: viewRect.top + (targetRect.top - groupRect.top),
+                left: viewRect.left + (targetRect.left - groupRect.left),
+                bottom: viewRect.bottom + (targetRect.bottom - groupRect.bottom),
+                right: viewRect.right + (targetRect.right - groupRect.right)
+            };
+        })
+        .on('dragmove', event => {
+            const {target} = event;
+            this.dragselect[this.view].removeSelectables(target);
+            this._moveElems(event.dx, event.dy);
+        })
+        .on('dragend', event => {
+            setTimeout(() => {
+                this.dragselect[this.view].addSelectables(event.target);
+            }, 10);
+            const vsnap1 = feezal.container.querySelector('#vsnap1');
+            const hsnap1 = feezal.container.querySelector('#hsnap1');
+            const vsnap2 = feezal.container.querySelector('#vsnap2');
+            const hsnap2 = feezal.container.querySelector('#hsnap2');
+
+            vsnap1.style.display = 'none';
+            hsnap1.style.display = 'none';
+            vsnap2.style.display = 'none';
+            hsnap2.style.display = 'none';
+
+            this.dragElement = null;
+            feezal.app.change();
+        })
+        .on('resizestart', event => {
+            // Console.log('resizestart', event.target)
+            this.resizeElement = event.target;
+            if (!event.target.classList.contains('feezal-selected')) {
+                this.dragselect[this.view].setSelection(event.target, true);
+            }
+        })
+        .on('resizemove', event => {
+            const {target} = event;
+
+            const changes = [];
+
+            let x = Number.parseFloat(target.style.left.replace('px', '')) || 0; // Target.xCoord || 0;
+            let y = Number.parseFloat(target.style.top.replace('px', '')) || 0; // Target.yCoord || 0;
+
+            const {width} = event.rect;
+            const {height} = event.rect;
+
+            // Update the element's style
+            if (target.style.width !== width + 'px') {
+                target.style.width = width + 'px';
+                changes.push('width');
+            }
+
+            if (target.style.height !== height + 'px') {
+                target.style.height = height + 'px';
+                changes.push('height');
+            }
+
+            // Translate when resizing from top or left edges
+            x += event.deltaRect.left;
+            y += event.deltaRect.top;
+
+            if (target.style.left !== x + 'px') {
+                target.style.left = x + 'px';
+                changes.push('left');
+            }
+
+            if (target.style.top !== y + 'px') {
+                target.style.top = y + 'px';
+                changes.push('top');
+            }
+
+            this.shadowRoot.querySelector('feezal-sidebar-inspector-styles').setStyle(target, changes);
+        })
+        .on('resizeend', event => {
+            setTimeout(() => {
+                this.dragselect[this.view].addSelectables(event.target);
+            }, 10);
+            this.resizeElement = null;
+            feezal.app.change();
+        });
 
         this.dragselect[this.view].addSelectables(element);
     }
 
+
+
     selectElement(element) {
-        this.dragselect[this.view].setSelection(element);
+        if (this.dragselect[this.view]) {
+            console.log('dragselect');
+            this.dragselect[this.view].setSelection(element);
+        } else if (element) {
+            element.classList.add('feezal-selected')
+        }
         this.updateSelection();
     }
 
